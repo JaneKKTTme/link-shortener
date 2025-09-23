42614dc7c9265fe6757d385beafe7625d4b12a67
Jane Khomenko <eva.khomenko.28@mail.ru>
2025-09-24 00:45:12 +0300

    Add usage of API to shorten URL-address

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from pyshorteners import Shortener
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def index():
-    return render_template('link_shorter_page.html')
+def shorten_link(long_link):
+    shortener = Shortener()
+    return shortener.clckru.short(long_link)
+
+
+def expand_link(short_link):
+    shortener = Shortener()
+    return shortener.clckru.expand(short_link)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def link_shorter_page():
+    output = None
+    if request.method == 'POST':
+        long_link = request.form.get('long_link', '')
+        output = shorten_link(long_link)
+    return render_template('link_shorter_page.html', output=output)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)    

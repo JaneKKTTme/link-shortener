4707f890fd07e2458e36cd70e07b97eb113394b9
Jane Khomenko <eva.khomenko.28@mail.ru>
2025-11-28 19:01:31 +0300

    Add the table creation

--- main.py
+++ main.py
@@ -13,10 +13,31 @@ if database_url and database_url.startswith("postgres://"):
 app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-with app.app_context():
-    db.create_all()
+init_db()
+
+def init_db():
+    try:
+        with app.app_context():
+            db.create_all()
+    except Exception as e:
+        if 'postgresql' in database_url:
+            try:
+                with app.context():
+                    create_table_sql = """
+                    CREATE TABLE IF NOT EXISTS shortened_links (
+                        id SERIAL PRIMARY KEY,
+                        long_link TEXT NOT NULL,
+                        short_link TEXT NOT NULL
+                    );
+                    """
+                    db.session.execute(db.text(create_table_sql))
+                    db.session.commit()
+            except Exception as e:
+                print(e)
 
 class ShortenedLink(db.Model):
+    __tablename__ = 'shortened_links'
+
     id = db.Column(db.Integer, primary_key=True)
     long_link = db.Column(db.Text, nullable=False)
     short_link = db.Column(db.Text, unique=True, nullable=False)
